8dca058f8ee9847a1f392b7ab0756677880cef01
slimaneguemil <sguemil>
2017-12-07 11:56:17 +0100

    testing CI build 3

--- src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
+++ src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
@@ -10,6 +10,6 @@ public class SpringCoreDevOpsApplication {
 		SpringApplication.run(SpringCoreDevOpsApplication.class, args);
 
 		//this is a whitespace change test
-		// another test... dede and ouioui
+		// another test... dede and ouioui and pepps
 	}
 }
